e579d3b38326024037388b8aaca8d457e08de90a
mertbasaryildirim <mertbasaryildirim@gmail.com>
2025-07-01 21:38:05 +0300

    Qt setup

---
+++ strack-gui/strack-gui/strack-gui.cpp
@@ -0,0 +1,24 @@
+﻿#include "strack-gui.h"
+#include <QApplication>
+#include <QLabel>
+#include <QVBoxLayout>
+
+StrackDialog::StrackDialog(QWidget* parent)
+    : QDialog(parent)
+{
+    setWindowTitle("Hello Dialog");
+
+    auto* layout = new QVBoxLayout(this);
+    auto* label = new QLabel("Hello, World!", this);
+    layout->addWidget(label);
+
+    setLayout(layout);
+}
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    StrackDialog dialog;
+    dialog.show();
+    return app.exec();
+}
---
+++ strack-gui/strack-gui/strack-gui.h
@@ -0,0 +1,14 @@
+﻿#ifndef STRACK_GUI_H
+#define STRACK_GUI_H
+
+#include <QDialog>
+
+class StrackDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit StrackDialog(QWidget* parent = nullptr);
+};
+
+#endif // STRACK_GUI_H
+
